975a6ea5b2a461e300b35c3424fd6408b18dd112
SemSchneider <489799@student.fontys.nl>
2024-12-09 21:52:11 +0100

    update banner en LO

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const images = document.querySelectorAll(".imgProjects img");
 
     const observerOptions = {
-        root: null, // Observer het viewport
-        rootMargin: "0px", // Geen extra marges
-        threshold: 0.5, // Het element moet 50% zichtbaar zijn voordat de animatie start
+        root: null, 
+        rootMargin: "0px", 
+        threshold: 0.5, 
     };
 
     const observer = new IntersectionObserver((entries) => {
@@ -12,10 +12,11 @@ document.addEventListener("DOMContentLoaded", () => {
             if (entry.isIntersecting) {
                 entry.target.classList.add("grow");
             } else {
-                entry.target.classList.remove("grow"); // Optioneel, animatie herhalen bij wegscrollen
+                entry.target.classList.remove("grow"); 
             }
         });
     }, observerOptions);
 
     images.forEach((img) => observer.observe(img));
 });
+
